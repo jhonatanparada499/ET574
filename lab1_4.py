b38020a4d28ea96ab720f363e4f35c74bb272ee0
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-08 01:46:59 +0000

    discounted price

---
+++ lab1_4.py
@@ -0,0 +1,8 @@
+#lab1_4.py - Discounted price - Jhonatan Parada Torres
+
+price = 99.99
+discountPersent = 25
+markdown = discountPersent / price * 100
+price = price - markdown
+
+print(f"Price = {round(price,2)}")
