289b8ce267c4801ac5972503391ae1151f6d2ce6
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-12 17:49:43 +0000

    update

--- Jhonatan_LAB1/lab1_5.py
+++ Jhonatan_LAB1/lab1_5.py
@@ -1,4 +1,5 @@
-#lab1_5.py - Miles per Galon a car averaged between two fillings - Jhonatan Parada Torres
+#lab1_5.py - Miles per Galon a car averaged between two fillings/
+# - Jhonatan Parada Torres
 
 #Customization
 initial_mile = 23456
@@ -9,6 +10,7 @@ gallon_used = 10
 distance_traveled = last_mile - initial_mile
 answer = distance_traveled / gallon_used
 
+#This line can be improved
 print(
     f"Distance Traveled: {distance_traveled} Miles\nGallon Used: {gallon_used} Gallon(s)\n"
     "How many miles per gallon did the car average between two fillings?\n"
