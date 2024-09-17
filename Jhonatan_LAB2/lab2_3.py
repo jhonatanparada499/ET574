75032af1990ea1d75e52881709591ba7e4529186
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-17 16:32:21 +0000

    Improved code readability

--- Jhonatan_LAB2/lab2_3.py
+++ Jhonatan_LAB2/lab2_3.py
@@ -1,6 +1,11 @@
 pyramid_height = 8
 
 for i in range(pyramid_height):
-    print(" " * (pyramid_height - (i + 1)),"*" * (i + (i + 1)),sep='',end='\n')
+    print(
+        " " * (pyramid_height - (i + 1)),
+        "*" * (i + (i + 1)),
+        sep='',
+        end='\n'
+        )
 
 
