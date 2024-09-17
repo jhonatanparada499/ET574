fa9cbe22b30e5305790a317b1ece1ca0916a39b8
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-17 17:50:47 +0000

    Improved pyramid documentation

--- Jhonatan_LAB2/lab2_3.py
+++ Jhonatan_LAB2/lab2_3.py
@@ -1,11 +1,30 @@
-pyramid_height = 8
+pyramid_rows = 5
 
-for i in range(pyramid_height):
-    print(
-        " " * (pyramid_height - (i + 1)),
-        "*" * (i + (i + 1)),
-        sep='',
-        end='\n'
-        )
+#0,1,2,3,4...
+#10,9,8,7...
+
+for i in range(pyramid_rows):
+    
+    #0,1,2,3,4,5
+
+    #white cell = w_cell = | |
+    #dark cell = d_cell = |x|
+    #pyramid rows = 3
+    #| || ||x| <-- row (i = 0) 
+    #| ||x||x| <-- row (i = 1)
+    #|x||x||x| <-- row (i = 2)
+    
+    w_cell = " "
+    d_cell = "*"
+
+    w_cells_per_row = pyramid_rows - (i + 1) 
+    d_cells_per_row = i + (i + 1)
+
+    row = [
+        w_cell * w_cells_per_row,
+        d_cell * d_cells_per_row
+    ]        
+
+    print(*row,sep='',end='\n')
 
 
