6eca3907e0d384388a20041e5f4614e6eb56ff23
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-19 17:56:47 +0000

    Improving pyramid logic

--- Jhonatan_LAB2/lab2_3.py
+++ Jhonatan_LAB2/lab2_3.py
@@ -1,30 +1,30 @@
 pyramid_rows = 5
-
-#0,1,2,3,4...
-#10,9,8,7...
+d_cell = "*"
+w_cell = "#"
 
 for i in range(pyramid_rows):
+
+    #How to invert the pyramid
+
+    d_cells_per_row = i + 1
+    w_cells_per_row = pyramid_rows - d_cells_per_row
+
+    row_version_1 = [
+      w_cell * w_cells_per_row,
+      d_cell * (d_cells_per_row + i),
+    ]
     
-    #0,1,2,3,4,5
-
-    #white cell = w_cell = | |
-    #dark cell = d_cell = |x|
-    #pyramid rows = 3
-    #| || ||x| <-- row (i = 0) 
-    #| ||x||x| <-- row (i = 1)
-    #|x||x||x| <-- row (i = 2)
+    row_version_2 = (
+        row_version_1 + [w_cell * w_cells_per_row]
+    )       
     
-    w_cell = " "
-    d_cell = "*"
+    print(row_version_2)
 
-    w_cells_per_row = pyramid_rows - (i + 1) 
-    d_cells_per_row = i + (i + 1)
+    #print(*row_version_1,end='\n')
 
-    row = [
-        w_cell * w_cells_per_row,
-        d_cell * d_cells_per_row
-    ]        
-
-    print(*row,sep='',end='\n')
-
-
+#white cell = w_cell = | |
+#dark cell = d_cell = |x|
+#pyramid rows = 3
+#| || ||x| <-- row (i = 0) 
+#| ||x||x| <-- row (i = 1)
+#|x||x||x| <-- row (i = 2)
