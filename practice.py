c35c5c1797646c86c0131c52677c8b09db4e1e26
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-19 17:57:06 +0000

    class work

--- practice.py
+++ practice.py
@@ -1,5 +1,30 @@
-num = 10
+celebrate = "hispanic heritage month"
+celebrate = celebrate.title()
 
-for i in range(num):
-    i *= 10
-    print(i)
+#Index, Slicing String Methods
+
+result_1 = (
+    f"'{
+        celebrate[
+            celebrate.index("s"):
+            celebrate.index("n") + 1
+        ]
+    }'"
+)
+
+result_2 = (
+    celebrate[
+        celebrate.index("o"):
+        celebrate.rindex("n") + 1
+        ]
+)
+
+result_2 = result_2.upper()
+
+#Careful when there is more than 
+#variable[x:y]
+#y >= x
+#variable[0:1] = variable[:1]
+#varible[1:2]
+
+print()
