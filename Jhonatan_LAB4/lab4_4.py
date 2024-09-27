89281a7af108fd409f4f87fc5e9c60bbe057d13a
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-09-27 18:09:18 +0000

    Documentation added

--- Jhonatan_LAB4/lab4_4.py
+++ Jhonatan_LAB4/lab4_4.py
@@ -1,8 +1,31 @@
-# logic
-# Only words for words longer than 2
+# README
+# This portion of the lab uses functions, lists with 
+# the method append(), slicing, built-in functions
+# such as: input(), len() and str(), string methods such as:
+# isspace(), strip() and index(), while loops, and if-else statements
+
+# LOGIC
+# 1. Imagine you have a string ' hello world '
+# 2. Use the strip method to remove the leading and trailing whitespace
+# 3. Now the string is 'hello world'
+# 4. Remove and save the first word somewhere [hello]
+# 5. Now the string is ' world'
+# 6. Go back to step 1 with the new string and repeat until string is ''
+
+# CHALLENGE
+# I believe this code is 100% functional, if you don't think so,
+# try to input any sentence with any amount of spaces and any amount
+# of words with any lenght. Were you able to break it?
+
+# IMPORTANT
+# 1. This code does not differentiate between numbers and letters,
+# so, for example, there will be 4 words in 'My id is 123'. 
+# 2. If you type a symbol alone such as 'hello . world', it will count
+# as a word, so the total of words would be 3, although '.' is a symbol.
 
 sntc = input("Enter a sentence: ") #sntc = sentence
 
+# functions are defined with the key word def and allows to reuse code
 def get_words(par):
     string = str(par) # forces the parameter to be a string (extra security)
     words_found = [] # will catch all the words it finds in the string
@@ -20,7 +43,8 @@ def get_words(par):
             
             string = '' # since the string is empty the loop stops at this point
 
-        else: # happens if there are yet spaces in the string
+        # happens if there are yet spaces in the string
+        else:
             new_word = string[:string.index(' ')] # defines always the first word in string
             words_found.append(new_word) # appends the new word
 
@@ -35,5 +59,6 @@ def get_words(par):
     
     return words_found # returns all the words found in the string as a list
 
-print(f'Number of words: {get_words(sntc)}')
+# displays the len of words_found
+print(f'Number of words: {len(get_words(sntc))}')
 
