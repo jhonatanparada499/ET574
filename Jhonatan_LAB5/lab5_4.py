3ce14f451666ce8ca2bb95ca1867f31b03dd86de
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-10-10 16:51:31 +0000

    nums_list simplified, num_padding created

--- Jhonatan_LAB5/lab5_4.py
+++ Jhonatan_LAB5/lab5_4.py
@@ -1,13 +1,15 @@
+num_padding = 4
 err_message = 'Invalid Input.'
 try:
     input_range = int(input("Enter a range: "))
     try:
         input_num = int(input("Enter an integer number: "))
-        nums_list = [num for num in range(1, input_range + 1)]
+        nums_list = list(range(1,input_range + 1)) # [num for num in range(1, input_range + 1)] #
 
         print(f'Multiplication Table of {input_num}')
         for num in nums_list:
-            print(f'{num}\t*\t{input_num}\t=\t{num * input_num}')
+            row = f'{num}\t*\t{input_num}\t=\t{num * input_num}'
+            print(row.expandtabs(num_padding)) 
 
     except ValueError:
         print(err_message)
