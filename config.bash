d9df5e1a81776d5f40a1c71b46f77a5c96ea5c2f
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-10-19 17:03:38 +0000

    automation added

---
+++ config.bash
@@ -0,0 +1,18 @@
+devName='Jhonatan Parada'
+labName='Jhonatan_LAB'
+labParent='Labs'
+labFilesNum=4
+
+lstLabName=$(ls $labParent | tail -n 1) # Jhonatan_LAB4
+lstLabNum=${lstLabName: -1} # 4
+
+newLabNum=$(expr $lstLabNum + 1)
+newLab=$labName$newLabNum # Jhonatan_LAB5
+
+mkdir $newLab 
+
+for i in $(seq 1 $labFilesNum); do fileName='lab'${newLabNum}_${i}.py && txt='# '${fileName}' - '${devName} && echo $txt > ${labParent}/${newLab}/${fileName}; done
+
+# newLab=${labName}$(expr $labNum + 1) # Jhonatan_LAB5
+
+devName='Jhonatan Parada' && labName='Jhonatan_LAB' && labParent='Labs' && labFilesNum=4 && lstLabName=$(ls $labParent | tail -n 1) && lstLabNum=${lstLabName: -1} && newLabNum=$(expr $lstLabNum + 1) && newLab=$labName$newLabNum && mkdir $newLab && for i in $(seq 1 $labFilesNum); do fileName='lab'${newLabNum}_${i}.py && txt='# '${fileName}' - '${devName} && echo $txt > ${labParent}/${newLab}/${fileName}; done
