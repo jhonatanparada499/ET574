1708d89dc1cd7ff16b29a835bed4574958811c1a
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-10-19 13:06:10 -0400

    Delete Jhonatan_LAB1 directory

--- Jhonatan_LAB1/lab1_1.py
+++
@@ -1,6 +0,0 @@
-#lab1_1.py - Displays a name in two different lines - Jhonatan Parada Torres
-
-name = 'Jhonatan'
-last_name = 'Parada'
-
-print(f'{name}\n{last_name}')
--- Jhonatan_LAB1/lab1_3.py
+++
@@ -1,7 +0,0 @@
-#lab1_3.py - Displays a quote - Jhonatan Parada Torres
-
-print(
-    'Albert Einstein once said,\n\
-    "A person who never made a mistake\n\
-    never tried anything new.'
-)
