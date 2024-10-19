84902529b4e926cf0ad0edd7c2667c8d3801fa23
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-10-19 19:49:19 +0000

    updated

--- createLab.sh
+++ createLab.sh
@@ -1,22 +1,23 @@
 devName='Jhonatan Parada';
 labName='Jhonatan_LAB';
 labParent='Labs';
-
-lstLabName=$(ls $labParent | tail -n 1); # Jhonatan_LAB4
-lstLabNum=${lstLabName: -1}; # 4
-
-newLabNum=$(expr $lstLabNum + 1);
-newLab=${labName}${newLabNum}; # Jhonatan_LAB5
+defaultFilesNum=4;
 
 files() {
-    local labFilesNum=${1:-4} # default num of files
-    
+    local labFilesNum=${1:-$defaultFilesNum} # default num of files
+
+    lstLabName=$(ls $labParent | tail -n 1); # Jhonatan_LAB4
+    lstLabNum=${lstLabName: -1}; # 4
+
+    newLabNum=$(expr $lstLabNum + 1);
+    newLab=${labName}${newLabNum}; # Jhonatan_LAB5
+
     mkdir ${labParent}/${newLab};
 
     for i in $(seq 1 $labFilesNum);
     do fileName='lab'${newLabNum}_${i}.py;
     txt='# '${fileName}' - '${devName};
     echo $txt > ${labParent}/${newLab}/${fileName};
-    done
+    done        
 }
 
