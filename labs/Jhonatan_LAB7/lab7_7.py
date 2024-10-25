82e3aeda34e958ab74c93f07b6dda902e31ba490
jhonatanparada499 <119643678+jhonatanparada499@users.noreply.github.com>
2024-10-25 02:40:22 +0000

    created

--- labs/Jhonatan_LAB7/lab7_7.py
+++ labs/Jhonatan_LAB7/lab7_7.py
@@ -1,1 +1,34 @@
 # lab7_7.py - Jhonatan Parada
+err_msg = 'Invalid Input'
+stop_char = '0'
+prompt = f'Enter a number or {stop_char} to stop: '
+nums_lst = []
+
+input_val = input(prompt)
+
+while input_val != stop_char:
+    try:
+        input_val = int(input_val)
+        nums_lst.append(input_val)
+        input_val = input(prompt)
+    except:
+        try:
+            input_val = float(input_val)
+            nums_lst.append(input_val)
+            input_val = input(prompt) 
+        except:
+            print(err_msg)
+            input_val = input(prompt)
+else:
+    if not nums_lst:
+        pass
+    else:
+        lst_sum = sum(nums_lst)
+        lst_averg = lst_sum / len(nums_lst)
+
+        if len(str(lst_averg)) <= 3: # minimumm float value is 0.1 (1 decimal) (3 characters)
+            lst_averg = int(lst_averg)
+
+        print(f'Sum = {lst_sum}')
+        print(f'Average = {lst_averg}')
+        print('Number (s) entered:\n',*nums_lst)
