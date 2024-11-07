410405335002c6b38d03a2630c60ce890a5386da
Jhonatan Parada <119643678+jhonatanparada499@users.noreply.github.com>
2024-11-07 18:21:53 +0000

    improving logic

--- labs/Jhonatan_LAB8/lab8_6.py
+++ labs/Jhonatan_LAB8/lab8_6.py
@@ -4,20 +4,13 @@ from random import randint
 def main():
     n = randint(1, 10)
     numList = list(range(1, n + 1))
+    print(numList)
     print(middle(numList))
 
 def middle(l):
     if not isinstance(l, list): return
-    
-    msg_1 = 'No changes made to list.'
-    msg_2 = 'List length'
-    lst_len = len(l)
-    new_lst = l.copy()
 
-    if lst_len <= 1: print(msg_1)
-    else: new_lst = new_lst[1:-1]
-
-    print(f'{msg_2} = {lst_len}', l, sep='\n')
-    return new_lst
+    if len(l) > 1: l = l[1:-1]
+    return l
 
 main()
